237ca431e597a2fb9e83db79878af4971f85d761
dinalzein <dha21@mail.aub.edu>
2022-04-17 22:36:27 +0300

    remove gpustat

--- utils/scripts/fine_tunning.sh
+++ utils/scripts/fine_tunning.sh
@@ -6,7 +6,6 @@
 #SBATCH --gres=gpu:1
 #SBATCH --exclude=calcul-gpu-lahc-2
 source /home_expes/tools/python/python3_gpu
-
 # Create environment
 python3 -m virtualenv .venv --python=python3.6
 
@@ -18,8 +17,6 @@ source .venv/bin/activate
 
 echo "CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
 echo "GPU Devices:"
-gpustat
-
 
 model_name=bert-base-cased
 block_size=256
@@ -28,8 +25,8 @@ for dataset in WikipediaGenderEvents CommonCrawl; do
 	for datatype in original neutral gender-swapped balanced pro-stereotype anti-stereotype; do
 		output_dir=transformer_models/${dataset}/${datatype}/fine-tuned
 		python language_modeling/run_language_modeling.py \
-			--model_name_or_path ${model_name} \
-			--output_dir ${output_dir} \
+		  --model_name_or_path ${model_name} \
+	          --output_dir ${output_dir} \
 		  --mlm \
 		  --do_train \
 		  --train_data_file data/${dataset}/${datatype}/full/full-train.txt  \
@@ -42,7 +39,7 @@ for dataset in WikipediaGenderEvents CommonCrawl; do
 		  --block_size ${block_size} \
 		  --save_steps=1000 \
 		  --num_train_epochs 30 \
-			--evaluation_strategy=epoch \
+		  --evaluation_strategy=epoch \
 		  --save_total_limit 30 \
 		  --seed 42 \
-			--report_to tensorboard
+		  --report_to tensorboard
